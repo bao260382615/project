e0869aa2cfe5494dfc322d0f450d74dbc1d6ad15
bao260382615 <260382615@qq.com>
2019-11-08 18:33:52 +0800

    æ³初始化项目
    git commit -m æ³初始化项目

---
+++ src/main/java/com/jk/Git1904Application.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1904Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1904Application.class, args);
+    }
+
+}
